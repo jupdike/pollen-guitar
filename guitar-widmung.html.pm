9ad691de1138652e67bcd3acd6a671245f3dcdc6
Jared Updike <jupdike@thebrain.com>
2019-09-23 15:04:42 -0700

    The remaining lyrics

--- guitar-widmung.html.pm
+++ guitar-widmung.html.pm
@@ -11,25 +11,72 @@
 
 ◊title{Widmung by Robert Schumann}
 
-◊desc{An attempt at an arrangement for guitar. Original key of Ab, tune guitar to Eb standard tuning (every string down a semitone). Capo fourth, fifth, sixth, or seventh fret to play in a different key for baritone vocal part. Original key of Ab has low note of B. Down 5 semitones (Key of Eb) is low note of Gb, down six (Key of D) is low note of F.}
+◊desc{An attempt at an arrangement for guitar. Original key of Ab, tune guitar to Eb standard tuning (every string down a semitone). Capo fourth, fifth, sixth, or seventh fret to play in a different key for baritone vocal part. Original key of Ab has low note of B (see "#"). Down 5 semitones (Key of Eb) is low note of Gb, down six (Key of D) is low note of F.}
 
 ◊song[
- ◊row[Ab  _ _ _   _ _        Ab _ _ _ _ _]
- ◊lyr["_ _ _ _ _ _           Du meine See- le du mein"]
+ ◊row[Ab  _ _ _   _ _          Ab _ _ _ _ _]
+ ◊lyr["*Key=Ab:* _ _ _ _ _             Du meine See- le du mein"]
  
- ◊row[Ab  _ _ _   _ _        Db _ _ Gb7 _ _]
- ◊lyr["Herz _ _ _ du meine   Wonn _ _ O du mein"]
+ ◊row[Ab  _ _ _   _ _          Db _ _ Gb7 _ _]
+ ◊lyr["Herz _ _ _ du meine     Wonn', _ _ O du mein"]
  
- ◊row[Bbm _ _  _ Db  _ Ab _ _ _   Eb7   _]
- ◊row[Eb7 _ Ab _ Ab7 _ Db _ _ _   Ab7/E _]
- ; TODO
+ ◊row[Bbm _ _  _ Db  _         Ab _ _ _   Eb7   _]
+ ◊lyr["Schmerz, _ _ _ du meine Welt, _ _ in der ich"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["le- _ be, mein Him- mel  du, _ _ da- rein ich"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["schwe- _ be, O du mein   Grab, _ _ in das hin-"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["ab ich e- _ _ wig        mein- nen Kum- _ _ mer"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["gab. _ _ _ _ _           *E:*_Du _ _ _ bist die"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["Ruh', _ _ _ du _         bist _ _ _ _ der"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["Frie- _ den#, _ _ _      du _ _ _ vist vom"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["Him- _ _ _ mel _         mir _ _ _ _ _ be-"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["schie- _ den. _ Dass du_mich   liebst, _ _ macht mich mir"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["wert, _ _ dein Blick hat       mich _ _ vor mir ver-"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["klärt, _ _ du hebst mich       *Ab:*_lie- _ _ bend ü- ber"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["mich, _ _ _ mein _       gu- ter Geist, mein bess- res"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["Ich! _ _ _ _ _           Du meine See- le, du mein"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["Herz, _ _ _ du meine     Wonn', _ _ o du mein"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["Schmerz, _ _ _ du meine  Welt, _ _ in der ich"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["le- _ be, mein Him- mel  du, _ _ da- rein ich"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["schwe- _ be, mein gu- ter    Geist, mein bess- _ _ res"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["Ich! _ _ _ _ _           _ _ _ _ _ _"]
+
+ ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr["_ _ _ _ _ _              _ _ _ _ _ _"]
 ]
-
-
-
-
-
-
 
 
 
