d9f7c98049665b016e65c5e8794ab41105775fe1
Jared Updike <jupdike@thebrain.com>
2019-09-24 13:06:03 -0700

    Initial commit for some guitar chords (Key of Ab parts) of Widmung

--- guitar-widmung.html.pm
+++ guitar-widmung.html.pm
@@ -1,86 +1,105 @@
 #lang pollen
 
 
-◊chord[Ab    (frets x 0 2 2 2 0)]
-◊chord[Db    (frets x x 0 2 3 2)]
-◊chord[Gb7   (frets 3 5 3 4 3 3)]
-◊chord[Bbm   (frets x 2 4 4 3 2)]
-◊chord[Eb7   (frets 0 2 0 1 0 0)]
-◊chord[Ab7   (frets x 0 2 0 2 0)]
-◊chord[Ab7/E (frets 0 0 2 0 2 3)]
+◊chord[Ab       (frets x 0 2 2 2 0)]
+◊chord[Ab7      (frets x 0 2 0 2 0)]
+◊chord[Ab7/Eb   (frets 0 0 2 0 2 3)]
+◊chord[Ab/Eb    (frets 0 0 2 2 2 0)]
+◊chord[Bbm      (frets x 2 4 4 3 2)]
+◊chord[Bbo7     (frets x 2 0 1 0 1)]
+◊chord[Bb7sus4+3(frets x 2 2 2 4 2)]
+◊chord[B7sus4+3 (frets x 2 2 2 4 2)]
+◊chord[Bo7/Eb   (frets 0 3 4 5 4 5)]
+◊chord[Dbo7     (frets x x 0 1 0 1)]
+◊chord[Cmin7no5 (frets x 4 x 6 5 7)]
+◊chord[Db       (frets x x 0 2 3 2)]
+◊chord[Dbm      (frets x x 0 2 3 1)]
+◊chord[Eb5      (frets 0 2 2 x 0 0)]
+◊chord[Eb7      (frets 0 2 0 1 0 0)]
+◊chord[Eb7sus4  (frets 0 2 0 2 0 0)]
+◊chord[Eb7b5no3 (frets 0 1 0 3 3 0)]
+◊chord[Ebsus4   (frets 0 2 2 2 0 0)]
+◊chord[F7/Eb    (frets 0 4 2 3 2 2)]
+◊chord[F7/A     (frets x 1 2 3 2 2)]
+◊chord[F/A      (frets x 1 4 x 2 2)]
+◊chord[F9/G     (frets 4 4 2 3 2 2)]
+◊chord[Gb7      (frets 3 5 3 4 3 3)]
+
+◊; played as B7sus4+3, but down a half-step = Bb7sus4+3
 
 ◊title{Widmung by Robert Schumann}
 
 ◊desc{An attempt at an arrangement for guitar. Original key of Ab, tune guitar to Eb standard tuning (every string down a semitone). Capo fourth, fifth, sixth, or seventh fret to play in a different key for baritone vocal part. Original key of Ab has low note of B (see "#"). Down 5 semitones (Key of Eb) is low note of Gb, down six (Key of D) is low note of F.}
 
 ◊song[
- ◊row[Ab  _ _ _   _ _          Ab _ _ _ _ _]
- ◊lyr["*Key=Ab:* _ _ _ _ _             Du meine See- le du mein"]
+ ◊row[Ab  _ _ _   _ _           Ab _ _ _ _ _]
+ ◊lyr{*Key=Ab:* _ _ _ _ _       Du meine See- le du mein}
+
+ ◊row[Ab  _ _ _   _ _           Db _ _ Gb7 _ _]
+ ◊lyr{Herz _ _ _ du meine       Wonn', _ _ O du mein} ; ' annoying syntax highlight problem
  
- ◊row[Ab  _ _ _   _ _          Db _ _ Gb7 _ _]
- ◊lyr["Herz _ _ _ du meine     Wonn', _ _ O du mein"]
- 
- ◊row[Bbm _ _  _ Db  _         Ab _ _ _   Eb7   _]
- ◊lyr["Schmerz, _ _ _ du meine Welt, _ _ in der ich"]
+ ◊row[Bbm _ _  _ Db  _          Ab _ _ _   Eb7   _]
+ ◊lyr{Schmerz, _ _ _ du meine   Welt, _ _ in der ich}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["le- _ be, mein Him- mel  du, _ _ da- rein ich"]
+ ◊row[_ _ Ab _ Ab7 _            Db _ _ _ Ab7/Eb _ ]
+ ◊lyr{le- _ be, mein Him- mel   du, _ _ da- rein ich}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["schwe- _ be, O du mein   Grab, _ _ in das hin-"]
+ ◊row[_ _ Db Gb7 _ _            Bbo7 _ _ Bbm _ _ ]
+ ◊lyr{schwe- _ be, O du mein    Grab, _ _ in das hin-}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["ab ich e- _ _ wig        mein- nen Kum- _ _ mer"]
+ ◊row[Ab _ _ _ _ Dbm            Ab _ Ebsus4 _ _ _ ]
+ ◊lyr{ab ich e- _ _ wig         mein- nen Kum- _ _ mer}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["gab. _ _ _ _ _           *E:*_Du _ _ _ bist die"]
+ ◊row[Ab _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr{gab. _ _ _ _ _            *E:*_Du _ _ _ bist die}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["Ruh', _ _ _ du _         bist _ _ _ _ der"]
+ ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊lyr{Ruh', _ _ _ du _          bist _ _ _ _ der} ; ' annoying syntax highlight problem
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["Frie- _ den#, _ _ _      du _ _ _ vist vom"]
+ ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊lyr{Frie- _ den#, _ _ _       du _ _ _ vist vom}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["Him- _ _ _ mel _         mir _ _ _ _ _ be-"]
+ ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊lyr{Him- _ _ _ mel _          mir _ _ _ _ _ be-}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["schie- _ den. _ Dass du_mich   liebst, _ _ macht mich mir"]
+ ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊lyr{schie- _ den. _ Dass du_mich   liebst, _ _ macht mich mir}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["wert, _ _ dein Blick hat       mich _ _ vor mir ver-"]
+ ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊lyr{wert, _ _ dein Blick hat       mich _ _ vor mir ver-}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["klärt, _ _ du hebst mich       *Ab:*_lie- _ _ bend ü- ber"]
+ ◊row[_ _ _ _ _ _               Eb7 _ _ _ _ _ ]
+ ◊lyr{klärt, _ _ du hebst mich  *Ab:*_lie- _ _ bend ü- ber}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["mich, _ _ _ mein _       gu- ter Geist, mein bess- res"]
+ ◊row[Cmin7no5 _ _ _ Ab/Eb _    Bb7sus4+3 _ _ _ _ _ ]
+ ◊lyr{mich, _ _ _ mein _        gu- ter Geist, mein bess- res}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["Ich! _ _ _ _ _           Du meine See- le, du mein"]
+ ◊row[Eb7 _ _ _ Eb7b5no3 _      Ab _ _ _ _ _ ]
+ ◊lyr{Ich! _ _ _ _ _            Du meine See- le, du mein}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["Herz, _ _ _ du meine     Wonn', _ _ o du mein"]
+ ◊row[Ab _ _ _ _ _              Db _ _ _ _ _ ]
+ ◊lyr{Herz, _ _ _ du meine      Wonn', _ _ o du mein}  ; ' annoying syntax highlight problem
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["Schmerz, _ _ _ du meine  Welt, _ _ in der ich"]
+ ◊row[Eb7 _ _ _ _ _             Ab _ _ _ Eb7 _ ]
+ ◊lyr{Schmerz, _ _ _ du meine   Welt, _ _ in der ich}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["le- _ be, mein Him- mel  du, _ _ da- rein ich"]
+ ◊row[_ _ Ab _ F7/Eb _          Bbm _ _ _ F7/A _ ]
+ ◊lyr{le- _ be, mein Him- mel   du, _ _ da- rein ich}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["schwe- _ be, mein gu- ter    Geist, mein bess- _ _ res"]
+ ◊row[F9/G _ Bbm F/A Bbm Dbo7    Ab/Eb Bo7/Eb Eb7sus4 _ _ Eb5 ]
+ ◊lyr{schwe- _ be, mein gu- ter  Geist, mein bess- _ _ res}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["Ich! _ _ _ _ _           _ _ _ _ _ _"]
+ ◊row[Ab _ _ _ _ _              _ _ _ _ _ _ ]
+ ◊lyr{Ich! _ _ _ _ _            _ _ _ _ _ _}
 
- ◊row[ _ _ _ _ _ _              _ _ _ _ _ _ ]
- ◊lyr["_ _ _ _ _ _              _ _ _ _ _ _"]
+ ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊lyr{_ _ _ _ _ _               _ _ _ _ _ _}
+
+ ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊lyr{_ _ _ _ _ _               _ _ _ _ _ _}
 ]
-
-
 
 
 ◊;items{
 ◊;item{You ◊b{wanted} it — you ◊i{got} it.}
-◊;item{◊link["https://google.com/search?q=racket"]{search for Racket}}  }
+◊;item{◊link{https://google.com/search?q=racket}{search for Racket}}  }
