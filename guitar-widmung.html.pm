94e650c3f0d0bc6f627f99fb99123d8be94853ed
Jared Updike <jupdike@thebrain.com>
2019-10-09 07:58:46 -0700

    Last (middle) measures of Widmung

--- guitar-widmung.html.pm
+++ guitar-widmung.html.pm
@@ -1,13 +1,14 @@
 #lang pollen
 
-
 ◊chord[G#min7b5 (frets 5 x 5 5 4 x)]
 ◊chord[Ab       (frets x 0 2 2 2 0)]
 ◊chord[Ab7      (frets x 0 2 0 2 0)]
 ◊chord[Ab7/Eb   (frets 0 0 2 0 2 3)]
 ◊chord[Ab/Eb    (frets 0 0 2 2 2 0)]
+◊chord[A        (frets 6 8 8 7 6 6)]
 ◊chord[Bbm      (frets x 2 4 4 3 2)]
 ◊chord[Bbo7     (frets x 2 0 1 0 1)]
+◊chord[B        (frets 8 x 5 5 5 x)]
 ◊chord[Bb7sus4+3(frets x 2 2 2 4 2)]
 ◊chord[B7sus4+3 (frets x 2 2 2 4 2)]
 ◊chord[Bo7/Eb   (frets 0 3 4 5 4 5)]
@@ -22,15 +23,27 @@
 ◊chord[Eb7b5no3 (frets 0 1 0 3 3 0)]
 ◊chord[Ebsus4   (frets 0 2 2 2 0 0)]
 ◊chord[E        (frets 1 3 3 2 1 1)]
+◊chord[Esus4    (frets 1 3 3 3 1 1)]
+◊chord[Eadd9    (frets x 8 7 5 8 x)]
 ◊chord[E/B      (frets x 3 3 2 1 1)]
+◊chord[E/B_5_8  (frets 8 8 x 5 6 5)]
 ◊chord[E7       (frets 1 3 1 2 1 1)]
+◊chord[Em7      (frets 1 3 1 1 1 1)] ◊; which is G/E
 ◊chord[A/E      (frets 1 1 3 3 3 1)]
 ◊chord[F7/Eb    (frets 0 4 2 3 2 2)]
 ◊chord[F7/A     (frets x 1 2 3 2 2)]
 ◊chord[F/A      (frets x 1 4 x 2 2)]
+◊chord[B/A      (frets 6 x 5 5 5 x)] ◊; which is B7
 ◊chord[F9/G     (frets 4 4 2 3 2 2)]
 ◊chord[F#min7   (frets 3 5 3 3 3 3)]
 ◊chord[Gb7      (frets 3 5 3 4 3 3)]
+◊chord[D/F#     (frets 3 x x 3 4 3)]
+◊chord[E/G#     (frets x 3 3 2 1 2)]
+◊chord[Am6      (frets 6 8 8 6 8 x)]
+◊chord[C+/A#    (frets 7 x 7 6 6 x)]
+◊chord[B7/F#    (frets 3 3 5 3 5 3)]
+◊chord[E7#5/G#  (frets 5 6 7 6 6 x)]
+
 
 ◊; played as B7sus4+3, but down a half-step = Bb7sus4+3
 
@@ -68,16 +81,16 @@
  ◊row[E _ E/B _ _ _             E _ G#min7b5 _ Ddim7 _ ]
  ◊lyr{Frie- _ den#, _ _ _       du _ _ _ bist vom}
 
- ◊row[F#min7 _ _ _ _ _          _ _ _ _ _ _ ]
- ◊lyr{Him- _ _ _ mel _          mir _ _ _ _ _ be-}
+ ◊row[F#min7 _ D/F# _ E/G# _    Am6 _ _ _ C+/A# _ ]
+ ◊lyr{Him- _ _ _ mel _          mir _ _ _ _ be-}
 
- ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
+ ◊row[E/B_5_8 _ B _ B/A _            Eadd9 _ _ _ B7/F# _ ]
  ◊lyr{schie- _ den. _ Dass du_mich   liebst, _ _ macht mich mir}
 
- ◊row[_ _ _ _ _ _               _ _ _ _ _ _ ]
- ◊lyr{wert, _ _ dein Blick hat       mich _ _ vor mir ver-}
+ ◊row[Esus4 _ E _ Em7 _         A _ _ _ E7#5/G# _ ]
+ ◊lyr{wert, _ _ dein Blick hat  mich _ _ vor mir ver-}
 
- ◊row[_ _ _ _ _ _               Eb7 _ _ _ _ _ ]
+ ◊row[_ _ A _ _ _               Eb7 _ _ _ _ _ ]
  ◊lyr{klärt, _ _ du hebst mich  *Ab:*_lie- _ _ bend ü- ber}
 
  ◊row[Cmin7no5 _ _ _ Ab/Eb _    Bb7sus4+3 _ _ _ _ _ ]
